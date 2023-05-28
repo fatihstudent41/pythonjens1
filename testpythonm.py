6678c00cebc00ebec10833d33a20e912c19c8f8a
fatihstudent41@gmail.com <93310233+achilles41@users.noreply.github.com>
2023-05-28 18:42:55 +0200

    my python jenkins try

---
+++ testpythonm.py
@@ -0,0 +1,10 @@
+print("test for jenkins")
+
+
+
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/fatihstudent41/pythonjens1.git
+#git push -u origin main
